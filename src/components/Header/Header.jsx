3abafa9220bbef2eed36766e1bbc20de07336928
ayush7654 <79410265+ayush7654@users.noreply.github.com>
2024-10-18 14:36:25 +0530

    Header updated for responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,18 @@
 import {useState,useContext} from "react";
 import {NavLink, Link, useLocation} from 'react-router-dom'
 import { Search } from "react-feather";
-/* import { BreadCrumbContext } from "../App"; */
+import './Header.css'
 export default function Header(){
 
     const elements = [{title:"HOME",path:'/'},
         {title:"STORE",path:'/store?page=1'},
-        {title:"YOUR CART", path:'/cart'},
+        {title:"CART", path:'/cart'},
         {title:"ABOUT", path:'/about'},
         {title:"LOGIN",path:'/login'}]
 
     const [searchTerm,setSearchTerm]= useState('')
     
-   /*  const [Breadcrumbs,setbreadcrumbs] = useContext(BreadCrumbContext) */
+  
 
     const handleClick=(title)=>{
         setbreadcrumbs(title)
@@ -26,19 +26,19 @@ export default function Header(){
 
    
     return(<div className="sticky-header">
-        <div className={location.pathname==='/'?"header-home":'header'}>
+        <div className="header-home">
         
-        <div style={{display:'flex', flexDirection:'row',alignItems:'center',justifyContent:'center',gap:'0px'}} className="logo-title">
-        <img src='/Logofinal3.png' width='72px' style={{paddingTop:'10px'}} />
-        <div style={{display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'center'}} className="logo-name">
+        <div className="logo-title">
+        <img src='/Logofinal3.png'  className="logoImg"  />
+        <div  className="logo-name">
         <div className="name">QuickMart</div>
-        <div style={{color:'white',paddingBottom:'20px'}}>Quick Deals, Quick Feels </div>
+        <div className="tagline" style={{color:'white',paddingBottom:'20px'}}>Quick Deals, Quick Feels </div>
         </div>
       
         </div>
 
-        <div style={{display:'flex', position:'relative',alignItems:'center'}}>
-            <input style={{ width: '600px',height:'40px',border: '0px' ,borderRadius:'4px'}} className="header-search-box" placeholder="What are you looking for?" value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}}/>
+        <div className="header-searchBar" >
+            <input  className="header-search-box" placeholder="What are you looking for?" value={searchTerm} onChange={(e)=>{setSearchTerm(e.target.value)}}/>
            <div style={{ position:'absolute',right:'10px'}} ><Link to={`/search/${searchTerm}` }> <Search style={{color:'grey'}}/></Link></div>
             
         </div>
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet, useLocation } from 'react-router-dom';
 
-import Header from './Header'
+import Header from './Header/Header'
 
 
 export default function Home(){
