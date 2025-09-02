59cc19f79f4725b0ed2096965bbf225bf2413b97
ayush7654 <79410265+ayush7654@users.noreply.github.com>
2025-09-03 02:59:26 +0530

    trends in fashion media query added

--- src/pages/Home/TrendingComponent/TrendingComponent.jsx
+++ src/pages/Home/TrendingComponent/TrendingComponent.jsx
@@ -9,9 +9,8 @@ export default function TrendingComponent() {
                 </div>  
               </div>
           <div className="trending-products">
-            <div id='trending-product'>
-              <div  className="trending-product-1"><span id ='tp-btn' className="tp-1-btn">WINTER COLLECTION</span></div></div>
-            <div id='trending-product'><div  className="trending-product-2"><span id ='tp-btn' className="tp-2-btn">SUMMER COLLECTION</span></div></div>
+            <div id='trending-product-div'> <div id='trending-product'  className="trending-product-1"><span id ='tp-btn' className="tp-1-btn">WINTER COLLECTION</span></div></div>
+            <div id='trending-product-div'><div id='trending-product' className="trending-product-2"><span id ='tp-btn' className="tp-2-btn">SUMMER COLLECTION</span></div></div>
 
 
           </div>
