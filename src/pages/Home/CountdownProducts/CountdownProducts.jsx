6c2cd044af3f3976b2fc64df5d6fe9d4c5abfc13
ayush7654 <79410265+ayush7654@users.noreply.github.com>
2025-09-02 20:45:28 +0530

    Browse Collections media query added

--- src/pages/Home/CountdownProducts/CountdownProducts.jsx
+++ src/pages/Home/CountdownProducts/CountdownProducts.jsx
@@ -4,7 +4,7 @@ import ProductCard from '../../../components/ProductCard'
 export default function CountdownProducts() {
   return (
       <div id='home-product-div' className="Countdown-home-Products-div">
-{/*        <div className='home-Products-window'></div> */}
+
          <div  className="home-head-div">
                 <div className="home-head-content"> 
                   <div className='home-head'>Cowntdown Deals.</div>
