b56138b6f26f4bc1178e9a67163a66cc6afbb301
ayush7654 <79410265+ayush7654@users.noreply.github.com>
2025-10-08 00:09:11 +0530

    explore card fixed

--- src/pages/Home/BrandComponent/BrandSM/BrandSM.jsx
+++ src/pages/Home/BrandComponent/BrandSM/BrandSM.jsx
@@ -5,11 +5,11 @@ export default function BrandSM() {
  const [Brands,setBrands] = useState(
     [
     {id:1,front:'BrandComponentIcons/QM-home-brand10.png', back:'BrandComponentIcons/Sony-product.jpeg', flipped:false},
-    {id:2,front:'BrandComponentIcons/QM-home-brand2.webp', back:'BrandComponentIcons/vans-product.webp', flipped:false},
-    {id:3,front:'BrandComponentIcons/QM-home-brand3.png', back:'BrandComponentIcons/Adidas-product.webp', flipped:false},
+    {id:2,front:'BrandComponentIcons/QM-home-brand2.webp', back:'BrandComponentIcons/Vans-product.jpg', flipped:false},
+    {id:3,front:'BrandComponentIcons/QM-home-brand3.png', back:'BrandComponentIcons/Adidas-product3.webp', flipped:false},
     {id:4,front:'BrandComponentIcons/QM-home-brand5.png', back:'BrandComponentIcons/Asus-product.jpg', flipped:false},
-    {id:5,front:'BrandComponentIcons/QM-home-brand4.png', back:'BrandComponentIcons/LouisV-product.jpg', flipped:false},
-    {id:6,front:'BrandComponentIcons/QM-home-brand6.png', back:'BrandComponentIcons/Chanel-product2.webp', flipped:false},
+    {id:5,front:'BrandComponentIcons/QM-home-brand4.png', back:'BrandComponentIcons/LV-product.avif', flipped:false},
+    {id:6,front:'BrandComponentIcons/QM-home-brand6.png', back:'BrandComponentIcons/Chanel-product.avif', flipped:false},
     {id:7,front:'BrandComponentIcons/QM-home-brand7.png', back:'BrandComponentIcons/samsung-product.jpg', flipped:false},
     {id:8,front:'BrandComponentIcons/QM-home-brand8.png', back:'BrandComponentIcons/Rolex-product.png', flipped:false},
     {id:9,front:'BrandComponentIcons/QM-home-brand9.png', back:'BrandComponentIcons/Decathlon-product.avif', flipped:false}
