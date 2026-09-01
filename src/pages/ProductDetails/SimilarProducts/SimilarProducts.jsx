49e5bcb52d7906d780e1f4d059ac4ebe5d2320d6
ayush7654 <79410265+ayush7654@users.noreply.github.com>
2026-09-01 20:11:31 +0530

    similar products query added

--- src/pages/ProductDetails/SimilarProducts/SimilarProducts.jsx
+++ src/pages/ProductDetails/SimilarProducts/SimilarProducts.jsx
@@ -5,23 +5,25 @@ import ScrollButton from '../../../components/ScrollingButton/ScrollingButton';
 import { MdStar} from 'react-icons/md';
 import { Link } from 'react-router-dom';
 import './SimilarProducts.css'
+import { Star } from 'lucide-react';
 export default function SimilarProducts({currentCategory,product}) {
 
 
   
   return (
      <div className="Product-Details-SimilarProducts">
-   <div className="sp-head-div">
-                
-   <div className=''>Similar Products</div>
-                  
-                  
-                
-            
-              </div>
+   <div  id='home-product-head'>
+    Similar Products
+         <div className="head-dot-wrapper">
+      <span className="head-dot red"></span>
+      <span className="head-dot blue" ></span>
+      <span className="head-dot green"></span>
+    
+    </div>
+    </div>
  <div className="Product-Details-SimilarProducts-list">
         
- {currentCategory ? currentCategory.products.slice(0,window.innerWidth>850?5:4).map((item,index) => 
+ {currentCategory ? currentCategory.products.slice(0,5).map((item,index) => 
   item.id !== product.id && (
 
     <div key={index} className='similar-product'>
@@ -34,19 +36,20 @@ export default function SimilarProducts({currentCategory,product}) {
 
       </div>
 
-       <Link to={`/store/${item.id}`} className='similar-shop-btn-wrapper'>
+    {/*    <Link to={`/store/${item.id}`} className='similar-shop-btn-wrapper'>
        <div className="similar-shop-btn">
         <ScrollButton text='Buy Now'/>
        </div>
 
-</Link> 
+</Link>  */}
       </div>
 
       <div className='similar-productDetails-div'>
       
        
               <div className="similar-pd-rating-wrapper">
-                <StarRating className='similar-pd-star' color='rgb(0, 0, 0)' rating={Math.round(item.rating)}/>
+       <StarRating className='similar-pd-star' color='rgb( 252, 186, 3)' rating={Math.round(item.rating)}/> 
+  
                 <span className='similar-pd-rating'>({item.rating.toFixed(1)})</span>
                 </div>
 
